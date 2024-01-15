33a27f68f5a75060f25f6499fad46cd69bd417be
Łukasz <lkanigowski@edu.cdv.pl>
2024-01-15 23:00:30 +0100

    New idea
    Basically a brand new website idea.

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('.btn').forEach(button => {
+        button.addEventListener('click', function(e){
+            e.preventDefault();
+            const sectionId = this.getAttribute('data-section');
+            document.querySelector('#' + sectionId).scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
