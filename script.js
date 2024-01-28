da57802fd45c4b5bab3a681f52eaf3393f1659c7
Łukasz <lkanigowski@edu.cdv.pl>
2024-01-28 22:33:42 +0100

    Added slider and calculator
    Commit: War Crimes

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ window.addEventListener('DOMContentLoaded', (event) => {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const deliveryTimeSlider = document.getElementById('deliveryTime');
+    const lengthSlider = document.getElementById('length');
+    const complexitySlider = document.getElementById('complexity');
+
+
+//code that multiples read values on click of a button and prints them out
+    document.getElementById('calculate').addEventListener('click', function () {
+        const deliveryTime = deliveryTimeSlider.value;
+        const length = lengthSlider.value;
+        const complexity = complexitySlider.value;
+
+        const result = parseFloat(350 * (1/deliveryTime) * length * (complexity * 0.5)).toFixed(2) + '€';
+        document.getElementById('result').innerHTML = result;
+
+    });
+
+
+});
