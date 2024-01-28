948b48f38548f925261b65bad7b46ea763204dbc
Łukasz <lkanigowski@edu.cdv.pl>
2024-01-28 23:00:01 +0100

    Calculator Finished
    Yikes.

--- script.js
+++ script.js
@@ -19,9 +19,33 @@ document.addEventListener("DOMContentLoaded", function () {
         const deliveryTime = deliveryTimeSlider.value;
         const length = lengthSlider.value;
         const complexity = complexitySlider.value;
+        var complexity_level = '';
 
         const result = parseFloat(350 * (1/deliveryTime) * length * (complexity * 0.5)).toFixed(2) + '€';
         document.getElementById('result').innerHTML = result;
+
+        switch (complexity) {
+            case '1':
+                complexity_level = 'really basic.';
+                break;
+            case '2':
+                complexity_level = 'a bit less complex.';
+                break;
+            case '3':
+                complexity_level = 'pretty standard.';
+                break;
+            case '4':
+                complexity_level = 'a bit more complex.';
+                break;
+            case '5':
+                complexity_level = 'really complex.';
+                break;
+        }
+
+        const description = "Your consultation takes place in " + deliveryTime + " days, it is " + length + " hours long and it is " + complexity_level;
+        document.getElementById('complexityDescription').innerHTML = description;
+        console.log(description);
+
 
     });
 
