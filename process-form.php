cdb5b63d492799a4ece348bdb5e8f9e0be4fcd3b
Łukasz <148983900+kvviinguwu@users.noreply.github.com>
2024-01-29 08:42:52 +0100

    Update process-form.php

--- process-form.php
+++ process-form.php
@@ -19,14 +19,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $mail->isSMTP();
         $mail->Host       = 'smtp.mailgun.org'; // Replace with your SMTP server
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'postmaster@sandbox6a84508d859a4b05920874c5b5e7bedd.mailgun.org'; // Replace with your SMTP username
-        $mail->Password   = '91220d7154d90a92de113dd209559b98-063062da-96ae47f7'; // Replace with your SMTP password
+        $mail->Username   = 'xxx'; // Replace with your SMTP username
+        $mail->Password   = 'xxx'; // Replace with your SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
         $mail->Port       = 587; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
         // Recipients
         $mail->setFrom($email, $fullname);
-        $mail->addAddress('kvviingu@gmail.com'); // Replace with the recipient's email address
+        $mail->addAddress('xxx'); // Replace with the recipient's email address
 
         // Content
         $mail->isHTML(false); // Set email format to plain text
